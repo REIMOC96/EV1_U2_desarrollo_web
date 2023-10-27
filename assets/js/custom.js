af00b42c0408e056471962d933a0acc1f2fee763
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-10-27 19:55:39 -0300

    aaaa

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,14 +6,16 @@ function comp_dat(){
     let telefono = document.querySelector("#telefono").valueAsNumber;
     let Carrera = document.querySelector("#Carrera").value;
     let Edad = document.querySelector("#edad").value;
-    let Msj = document.querySelector("#txt_ar")
-
+    let Msj = document.querySelector("#txt_ar").value;
+    let valid = false;
+    var conf = confirm("¿los datos son correctos?")
     //vamos con lo chungo D:<
     //tuve que leer muchisima documentacion pa esto, pero entendi muchisimo O.O
     //comprobamos los numeros en el nombre
     
     //el try lo agrege porque asi veo si fue todo bien o no uwu
     try{
+        
         //vamos con el nombre primero
         //esto me tomo un Rato VV
         if(!/^([a-z; A-Z])*$/.test(nombre))
@@ -29,7 +31,7 @@ function comp_dat(){
         if(apellido.length == 0){
                 alert("porfavor ingrese su apellido")
                 }
-            if(!/^([a-z;A-Z;"ñ"])*$/.test(apellido))
+            else if(!/^([a-z;A-Z;"ñ"])*$/.test(apellido))
             alert("no se admiten numeros en el apellido");
             
 
@@ -48,6 +50,7 @@ function comp_dat(){
             else if( telefono > 999999999 ){
                 alert("el telefono debe tener un valor menor que 999999999")
             }
+
             /*
             else{
                 alert("todo bien con el telefono we")
@@ -57,7 +60,7 @@ function comp_dat(){
         //esto me tomo tiempo VV
         //comprobar carrera :
         if(!/^([a-z; A-Z])*$/.test(Carrera))
-            alert("no se admiten numeros en el nombre de la carrera");
+            alert("no se admiten numeros en la carrera");
             //comprobamos que no este vacia la carrera 
             else if(Carrera.length == 0 ){
             alert("porfavor ingrese la carrera que le interesa");
@@ -73,17 +76,22 @@ function comp_dat(){
         //asi evitamos que mande siempre los mensajes de alerta
         // de la edad minima y maxima VVV
         else{
-            if (Edad <= 16){
-                alert("La Edad minima es de 17 años");
-            }
-            if(Edad >= 60 ){
-                alert("La edad maximas es de 60 años");
+            if (Edad <= 16 || Edad >= 60 ){
+                alert("ingrese una edad entre 17 y 60 años");
             }
         }
-    } 
+        if(Msj.length == 0){
+            alert("porfavor ingrese su un mensaje o consulta")
+            }
+        else if(!/^([1-9])*/.test(Msj))
+        alert("porfavor ingrese una consulta o mensaje valido");
+        }
+
     // lo del try - error, me sirvio muchisimo, usar mas seguido. 
-    catch(error){
+    catch {
         alert("algo malio sal")
+
+
+    //no pude hacer lo de confirmar todo uno por uno y luego hacer que me mande un mensaje de todo bien :c
     }
-    
-}
+    }
